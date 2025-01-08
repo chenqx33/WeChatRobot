680dbee3582a0039bc9871481a3cc44c4d9beec3
chenqixin <chenqixin@bytedance.com>
2025-01-08 19:45:24 +0800

    通用

--- plugin/__init__.py
+++ plugin/__init__.py
@@ -1,9 +1,22 @@
 from .plugin_managment import PluginManager
 from .base_plugin import PluginFather
-from .impl.ban_plugin import BanPlugin
-from .impl.auth_plugin import AuthPlugin
-from .impl.sys_cmd import SystemPlugin
-from .impl.safe import SafePlugin
+
+import pkgutil
+import importlib
+
+def import_submodules(package, recursive=False):
+    """Import all submodules of a package."""
+    importlib.invalidate_caches()
+    submodules = []
+    for loader, name, is_pkg in pkgutil.iter_modules(package.__path__):
+        full_name = package.__name__ + '.' + name
+        submodule = importlib.import_module(full_name)
+        submodules.append(submodule)
+        if recursive and is_pkg:
+            submodules.extend(import_submodules(submodule))
+    return submodules
+
+import_submodules(importlib.import_module('impl', package='.impl'))
 
 
 # 其他插件模块...
