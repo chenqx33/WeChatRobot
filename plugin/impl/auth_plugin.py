d9db5ba3e75c64b7943ca5c6f7fd890a9b697180
chenqixin <chenqixin@bytedance.com>
2025-01-08 14:23:02 +0800

    bugfix 2

--- plugin/impl/auth_plugin.py
+++ plugin/impl/auth_plugin.py
@@ -6,12 +6,16 @@ from plugin.action_enum import ActionEnum
 
 from plugin.plugin_context import PluginContext
 
-
+import logging
 class AuthPlugin(PluginFather):
     def __init__(self):
         super().__init__(-1, [StageEnum.PRE_PROCESS], False)
 
     def do_handle(self, msg: WxMsg, wcf: Wcf) -> PluginContext:
+        logging.info(f'{type(self.admins)}')
+        logging.info(f'{type(msg.sender())}')
+        self.admins.append('abc')
+        logging.info('f admins:{self.admins}')
         if msg.content.startswith("/auth") and not not msg.from_group():
             if msg.content.endswith("/1818"):
                 self.admins.append(msg.sender())
